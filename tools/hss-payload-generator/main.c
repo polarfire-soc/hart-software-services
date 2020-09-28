56fb19af152f64218e2701fcd21bf5e7da42b0f1
Ivan Griffin <ivan.griffin@emdalo.com>
2020-09-28 15:42:15 +0100

    Changing version number of payload generator
    Changing version number of payload generator to sync up with the HSS release version.

--- tools/hss-payload-generator/main.c
+++ tools/hss-payload-generator/main.c
@@ -38,7 +38,7 @@
 #include "dump_payload.h"
 #include "debug_printf.h"
 
-#define GEN_VERSION_STRING "20.09.1"
+#define GEN_VERSION_STRING "0.99.11.1"
 
 struct HSS_BootImage bootImage;
 struct HSS_BootChunkDesc *pChunkDescs;
