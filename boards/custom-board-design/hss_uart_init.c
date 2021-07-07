3c155ce74a13c5926ce27568a6ef5ada70a561a7
Cyril Jean <cyril.jean@microchip.com>
2021-07-07 12:56:42 +0100

    boards/custom-boad-design: Added new HSS build target for user modifed designs and/or custom board bring-up without affected known good build targets.

---
+++ boards/custom-board-design/hss_uart_init.c
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright 2019-2021 Microchip Corporation.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * MPFS HSS Embedded Software
+ *
+ */
+
+/**
+ * \file HSS Debug UART Initalization
+ * \brief Debug UART Initialization
+ */
+
+#include "config.h"
+#include "hss_types.h"
+#include "hss_init.h"
+
+#include <assert.h>
+
+#include "hss_debug.h"
+
+#include "drivers/mss_uart/mss_uart.h"
+
+bool HSS_UARTInit(void)
+{
+    // initialise debug UART
+
+#if IS_ENABLED(CONFIG_PLATFORM_MPFS)
+    MSS_UART_init(&g_mss_uart0_lo, MSS_UART_115200_BAUD,
+        MSS_UART_DATA_8_BITS | MSS_UART_NO_PARITY | MSS_UART_ONE_STOP_BIT);
+
+    // default all UARTs to 115200 for now
+    // subsequent OS loads can change these if needed...
+    MSS_UART_init(&g_mss_uart1_lo, MSS_UART_115200_BAUD,
+        MSS_UART_DATA_8_BITS | MSS_UART_NO_PARITY | MSS_UART_ONE_STOP_BIT);
+
+    MSS_UART_init(&g_mss_uart2_lo, MSS_UART_115200_BAUD,
+        MSS_UART_DATA_8_BITS | MSS_UART_NO_PARITY | MSS_UART_ONE_STOP_BIT);
+
+    MSS_UART_init(&g_mss_uart3_lo, MSS_UART_115200_BAUD,
+        MSS_UART_DATA_8_BITS | MSS_UART_NO_PARITY | MSS_UART_ONE_STOP_BIT);
+
+    MSS_UART_init(&g_mss_uart4_lo, MSS_UART_115200_BAUD,
+        MSS_UART_DATA_8_BITS | MSS_UART_NO_PARITY | MSS_UART_ONE_STOP_BIT);
+#else
+#  error Unknown PLATFORM
+#endif
+
+    return true;
+}
---
+++ boards/custom-board-design/mpfs_hal_config/mss_sw_config.h
@@ -0,0 +1,153 @@
+#ifndef USER_CONFIG_MSS_USER_CONFIG_H_
+#define USER_CONFIG_MSS_USER_CONFIG_H_
+
+/*******************************************************************************
+ * Copyright 2019-2021 Microchip FPGA Embedded Systems Solutions.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * MPFS HAL Embedded Software
+ *
+ */
+
+/*******************************************************************************
+ *
+ * Platform definitions
+ * Version based on requirements of MPFS MSS
+ *
+ */
+ /*========================================================================*//**
+  @mainpage Sample file detailing how mss_sw_config.h should be constructed for
+    the MPFS MSS
+
+    @section intro_sec Introduction
+    The mss_sw_config.h is to be located in the project
+    ./src/platform/config/software/mpfs_hal directory.
+    This file must be hand crafted when using the MPFS MSS.
+
+
+    @section
+
+*//*==========================================================================*/
+
+
+/*------------------------------------------------------------------------------
+ * MPFS_HAL_FIRST_HART and MPFS_HAL_LAST_HART defines used to specify which
+ * harts to actually start.
+ * Set MPFS_HAL_FIRST_HART to a value other than 0 if you do not want your code
+ * to start and execute code on the E51 hart.
+ * Set MPFS_HAL_LAST_HART to a value smaller than 4 if you do not wish to use
+ * all U54 harts.
+ * Harts that are not started will remain in an infinite WFI loop unless used
+ * through some other method
+ */
+#ifndef MPFS_HAL_FIRST_HART
+#define MPFS_HAL_FIRST_HART  0
+#endif
+
+#ifndef MPFS_HAL_LAST_HART
+#define MPFS_HAL_LAST_HART   4
+#endif
+
+/*------------------------------------------------------------------------------
+ * Markers used to indicate startup status of hart
+ */
+#define HLS_DATA_IN_WFI                     0x12345678u
+#define HLS_DATA_PASSED_WFI                 0x87654321u
+
+/*------------------------------------------------------------------------------
+ * Define the size of the HLS used
+ * In our HAL, we are using Hart Local storage for debug data storage only
+ * as well as flags for wfi instruction management.
+ * The TLS will take memory from top of the stack if allocated
+ *
+ */
+#define HLS_DEBUG_AREA_SIZE     64
+
+/* define the required tick rate in Milliseconds */
+/* if this program is running on one hart only, only that particular hart value
+ * will be used */
+#define HART0_TICK_RATE_MS  5UL
+#define HART1_TICK_RATE_MS  5UL
+#define HART2_TICK_RATE_MS  5UL
+#define HART3_TICK_RATE_MS  5UL
+#define HART4_TICK_RATE_MS  5UL
+
+#define H2F_BASE_ADDRESS 0x20126000     /* or 0x28126000 */
+
+/*
+ * define how you want the Bus Error Unit configured
+ */
+#define BEU_ENABLE                  0x0ull
+#define BEU_PLIC_INT                0x0ull
+#define BEU_LOCAL_INT               0x0ull
+
+/*
+ * MPFS_HAL_HW_CONFIG
+ * Conditional compile switch is used to determine if MPFS HAL will perform the
+ * hardware configurations or not.
+ * Defined      => This program acts as a First stage bootloader and performs
+ *                 hardware configurations.
+ * Not defined  => This program assumes that the hardware configurations are
+ *                 already performed (Typically by a previous boot stage)
+ *
+ * List of items initialised when MPFS_HAL_HW_CONFIG is enabled
+ * - load virtual rom (see load_virtual_rom(void) in system_startup.c)
+ * - l2 cache config
+ * - Bus error unit config
+ * - MPU config
+ * - pmp config
+ * - I/O, clock and clock mux's, DDR and SGMII
+ * - will start other harts, see text describing MPFS_HAL_FIRST_HART,
+ *   MPFS_HAL_LAST_HART above
+ */
+#ifndef MPFS_HAL_HW_CONFIG
+#define MPFS_HAL_HW_CONFIG
+#endif
+
+/*
+ * If not using item, comment out line below
+ */
+#define SGMII_SUPPORT
+#define DDR_SUPPORT
+#define MSSIO_SUPPORT
+
+/*
+ * DDR software options
+ */
+
+/*
+ * Debug DDR startup through a UART
+ * Comment out in normal operation. May be useful for debug purposes in bring-up
+ * of a new board design.
+ * See the weak function setup_ddr_debug_port(mss_uart_instance_t * uart)
+ * If you need to edit this function, make a copy of of the function without the
+ * weak declaration in your application code.
+ * */
+//#define DEBUG_DDR_INIT
+//#define DEBUG_DDR_RD_RW_FAIL
+//#define DEBUG_DDR_RD_RW_PASS
+//#define DEBUG_DDR_CFG_DDR_SGMII_PHY
+//#define DEBUG_DDR_DDRCFG
+/*
+* We need to redefine the following AXI address range if set incorrectly
+* This is the case for Libero 12.5 and Libero 12.6
+*
+* LIBERO_SETTING_CFG_AXI_END_ADDRESS_AXI1_0 is the definition for cached axi addrress
+* LIBERO_SETTING_CFG_AXI_END_ADDRESS_AXI2_0 is the address for non-cached Libero address
+* 0x7FFFFFFFUL => 2 GB address range
+*
+*/
+#define LIBERO_SETTING_CFG_AXI_END_ADDRESS_AXI1_0    0x7FFFFFFFUL
+#define LIBERO_SETTING_CFG_AXI_END_ADDRESS_AXI1_1    0x00000000UL
+#define LIBERO_SETTING_CFG_AXI_END_ADDRESS_AXI2_0    0x7FFFFFFFUL
+#define LIBERO_SETTING_CFG_AXI_END_ADDRESS_AXI2_1    0x00000000UL
+
+/*
+ * DDR Patch settings - to force RMW
+ */
+#define LIBERO_SETTING_DDRPHY_MODE		0x00014A24UL
+#define LIBERO_SETTING_CFG_DM_EN		0x00000000UL
+#define LIBERO_SETTING_CFG_RMW_EN		0x00000001UL
+
+#endif /* USER_CONFIG_MSS_USER_CONFIG_H_ */
