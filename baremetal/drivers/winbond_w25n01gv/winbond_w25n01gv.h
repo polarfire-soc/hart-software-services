1830d9c30734358d88e41770f32694dd1be8e07f
Ivan Griffin <ivan.griffin@microchip.com>
2023-02-22 10:11:49 +0000

    chore(copyright): making copyrights consistent
    Ensuring copyright is consistent with other files

--- baremetal/drivers/winbond_w25n01gv/winbond_w25n01gv.h
+++ baremetal/drivers/winbond_w25n01gv/winbond_w25n01gv.h
@@ -1,5 +1,5 @@
 /***************************************************************************//**
- * Copyright 2022 Microchip Corporation.
+ * Copyright 2023 Microchip FPGA Embedded Systems Solutions.
  *
  * SPDX-License-Identifier: MIT
  *
