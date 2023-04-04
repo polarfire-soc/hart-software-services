abd447422fb38e48962061731f7a03646d90291a
Conor Dooley <conor.dooley@microchip.com>
2023-04-04 09:02:28 +0100

    fix(sys_services): fix compilation issues with v2.1.100 driver
    Fix the following issues:
    mss_sys_services.c:47 col 1: error: 'static' is not at beginning of declaration [-Werror=old-style-declaration]
    mss_sys_services.c:1762 col 13: error: variable 'mb_format' set but not used [-Werror=unused-but-set-variable]
    mss_sys_services.c:2165 col 23: error: variable 'reg' set but not used [-Werror=unused-but-set-variable]
    
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- baremetal/polarfire-soc-bare-metal-library/src/platform/drivers/mss/mss_sys_services/mss_sys_services.c
+++ baremetal/polarfire-soc-bare-metal-library/src/platform/drivers/mss/mss_sys_services/mss_sys_services.c
@@ -44,7 +44,7 @@ static uint16_t g_int_service_response_size;
 static uint16_t g_int_service_response_offset;
 static uint16_t g_mb_offset;
 
-volatile static uint8_t g_message_interrupt_counter = 0u;
+static volatile uint8_t g_message_interrupt_counter = 0u;
 
 /*******************************************************************************
  * Callback handler function declaration
@@ -1759,17 +1759,10 @@ MSS_SYS_unlock_debug_passcode
 )
 {
     uint16_t status = MSS_SYS_PARAM_ERR;
-    uint8_t mb_format[32] = {0};
-    uint8_t index = 0u;
 
     if (cmd_data == NULL_BUFFER)
     {
         return status;
-    }
-
-    for (index = 0u; index < 32u; index++)
-    {
-        mb_format[index] = cmd_data[index];
     }
 
     if (MSS_SYS_SERVICE_INTERRUPT_MODE == g_service_mode)
@@ -2162,7 +2155,7 @@ g5c_message_plic_IRQHandler
 {
     g_message_interrupt_counter++;
 
-    volatile uint32_t reg = *MSS_SCBMESSAGE; /* read message reg. */
+    volatile uint32_t __attribute__((unused)) reg = *MSS_SCBMESSAGE; /* read message reg. */
     reg = *MSS_SCBMESSAGE_INT;
     *MSS_SCBMESSAGE_INT = 0x0u; /* clear message_int reg */
     reg = *MSS_SCBMESSAGE_INT;
