075e1de2dad8d913a3f5bd214c719ceb957367e9
Ivan Griffin <ivan.griffin@microchip.com>
2023-05-31 20:38:54 +0100

    chore(version): increment HSS version number

--- include/hss_version.h
+++ include/hss_version.h
@@ -40,7 +40,7 @@ extern "C" {
 
 #define HSS_VERSION_MAJOR 0
 #define HSS_VERSION_MINOR 99
-#define HSS_VERSION_PATCH 35
+#define HSS_VERSION_PATCH 36
 
 #ifndef VENDOR_STRING
 #  define VENDOR_STRING "dev-build"
